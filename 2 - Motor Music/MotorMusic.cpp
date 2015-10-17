662ba825d3af5ca97ca0953c6982d75a11eced7e
Miguel <santosmiguel25@gmail.com>
2015-10-17 02:06:09 +0100

    Music generation added

--- 2 - Motor Music/MotorMusic.cpp
+++ 2 - Motor Music/MotorMusic.cpp
@@ -1,16 +1,42 @@
 #include "sfr62p.h"
+#include <stdlib.h>
 
 #define MOTOR_LEFT(delay)  P7_6 = 1; P8_0 = 0; for(int i=0;i<delay;i++);
 #define MOTOR_RIGHT(delay) P7_6 = 0; P8_0 = 1; for(int i=0;i<delay;i++);
 #define MOTOR_VIBRATE(delay) MOTOR_LEFT(delay); MOTOR_RIGHT(delay);
 #define MOTOR_STOP() P7_6 = P8_0 = 0;
 
-#define MAX_MOTOR_DB 500
-#define MIN_MOTOR_DB 0
+#define MAX_MOTOR_PITCH 500
+#define MIN_MOTOR_PITCH 0
 
-void tone(int db, int duration_ms) {
+void tone_us(int pitch, int duration_us) {
 	/* Check for invalid tones */
-	if(db > MAX_MOTOR_DB || db < MIN_MOTOR_DB) return;
+	if(pitch > MAX_MOTOR_PITCH || pitch < MIN_MOTOR_PITCH) return;
+
+	for(; duration_us; duration_us--) {
+		/* Restart timer */
+		TA1IC = 0;
+		TA1MR = 0x42;
+		TA1 = 0x02;
+		TABSR_1 = 1;
+		ONSF_1 = 1;
+		
+		for(;;) {
+			/* Play the motor's sound */
+			MOTOR_VIBRATE(pitch);
+			
+			/* Check when the timer has finished (should be 1ms) */
+			if(TA1IC_3) break;
+		}
+		TABSR_1 = 1;
+	}
+	
+	MOTOR_STOP();	
+}
+
+void tone(int pitch, int duration_ms) {
+	/* Check for invalid tones */
+	if(pitch > MAX_MOTOR_PITCH || pitch < MIN_MOTOR_PITCH) return;
 
 	for(; duration_ms; duration_ms--) {
 		/* Restart timer */
@@ -22,7 +48,7 @@ void tone(int db, int duration_ms) {
 		
 		for(;;) {
 			/* Play the motor's sound */
-			MOTOR_VIBRATE(db);
+			MOTOR_VIBRATE(pitch);
 			
 			/* Check when the timer has finished (should be 1ms) */
 			if(TA1IC_3) break;
@@ -34,14 +60,10 @@ void tone(int db, int duration_ms) {
 }
 
 void main(void) {
+	/* Initialize motor's direction pins as outputs: */
 	P7D_6 = P8D_0 = 1;
 	
-	/* Laser sounds! */
-	
-	for(;;) {
-		for(int i=0;i<MAX_MOTOR_DB;i+=5)
-			tone(i, 1);
-		for(int i=MAX_MOTOR_DB;i>=MIN_MOTOR_DB;i-=5)
-			tone(i, 1);
-	}
+	/* Ultimate music generator! */
+	for(;;)
+		tone(rand()%500, rand()%100+1);
 }
